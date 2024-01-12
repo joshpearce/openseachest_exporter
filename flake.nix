6852d2b823081f9885a385e502e92e021b65bc0f
Joshua J. Pearce <joshua.pearce@gmail.com>
2024-01-12 13:31:47 -0500

    pin rust toochain version in flake

--- flake.nix
+++ flake.nix
@@ -5,16 +5,33 @@
     flake-utils.inputs.systems.follows = "systems";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs, systems }:
+  outputs = { self, flake-utils, naersk, nixpkgs, systems, nixpkgs-mozilla }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
+
+          overlays = [
+            (import nixpkgs-mozilla)
+          ];
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        toolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain;
+          sha256 = "sha256-SXRtAuO4IqNOQq+nLbrsDFbVk+3aVA8NNpSZsKlVH/8=";
+        }).rust;
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
 
       in rec {
         # For `nix build` & `nix run`:
